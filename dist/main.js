207d772ab5f94cf4ecb9ab7640b9f4efa5d4fb47
anis-king-911 <91683396+anis-king-911@users.noreply.github.com>
2025-04-09 00:27:34 +0100

    Delete dist directory

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-import LoadData from './javascript/ReadData.js';
-
-const tBody = document.querySelector('tbody');
-const Boxs = document.querySelectorAll('.box');
-const Score = document.querySelector('.Goals');
-
-window.addEventListener('DOMContentLoaded', () => LoadData(tBody, Boxs, Score))
